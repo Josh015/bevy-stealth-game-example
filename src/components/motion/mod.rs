d36d3c37c7d2abc2913deb7478082b88eb8692f4
josh015 <joshua.d.ols@gmail.com>
2024-03-18 22:33:15 -0400

    Combined `angular_velocity` into `velocity` module.

--- src/components/motion/mod.rs
+++ src/components/motion/mod.rs
@@ -1,11 +1,9 @@
 use bevy::prelude::*;
 
-mod angular_velocity;
 mod mover;
 mod speed;
 mod velocity;
 
-pub use angular_velocity::*;
 pub use mover::*;
 pub use speed::*;
 pub use velocity::*;
@@ -14,6 +12,6 @@ pub(super) struct MotionPlugin;
 
 impl Plugin for MotionPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((AngularVelocityPlugin, MoverPlugin, VelocityPlugin));
+        app.add_plugins((MoverPlugin, VelocityPlugin));
     }
 }
--- src/components/motion/velocity.rs
+++ src/components/motion/velocity.rs
@@ -4,13 +4,34 @@ pub(super) struct VelocityPlugin;
 
 impl Plugin for VelocityPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, velocity);
+        app.add_systems(Update, (angular_velocity, velocity).chain());
     }
+}
+
+/// Rotational velocity which updates rotation over time.
+#[derive(Clone, Component, Debug)]
+pub struct AngularVelocity {
+    pub axis: Direction3d,
+    pub velocity: f32,
 }
 
 /// Velocity that updates translation over time.
 #[derive(Clone, Component, Debug)]
 pub struct Velocity(pub Vec3);
+
+fn angular_velocity(
+    time: Res<Time>,
+    mut query: Query<(&AngularVelocity, &mut Transform)>,
+) {
+    for (angular_velocity, mut transform) in &mut query {
+        transform.rotation = (transform.rotation
+            * Quat::from_axis_angle(
+                *angular_velocity.axis,
+                angular_velocity.velocity * time.delta_seconds(),
+            ))
+        .normalize();
+    }
+}
 
 fn velocity(time: Res<Time>, mut query: Query<(&Velocity, &mut Transform)>) {
     for (velocity, mut transform) in &mut query {
