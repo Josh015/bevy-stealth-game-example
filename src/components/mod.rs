8ad00235c866f2305faadbf291b7a09f148517a1
josh015 <joshua.d.ols@gmail.com>
2024-03-19 21:35:07 -0400

    Flattened `components` folder.

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -3,22 +3,70 @@
 
 use bevy::app::prelude::*;
 
-mod actors;
+mod animation_clips;
+mod blocks_vision;
+mod deflects_sounds;
+mod door;
+mod drop_shadow;
+mod equipped_firearm;
+mod footsteps;
+mod guard;
+mod hearing;
+mod invisibility;
 mod mover;
+mod panning;
 mod physics;
-mod status_effects;
+mod pickup;
+mod player;
+mod quiet_footsteps;
+mod security_camera;
+mod shatterable;
+mod spawn_point;
+mod start_location;
+mod stunnable;
 mod trigger;
+mod vision;
+mod wall;
+mod waypoint;
+mod weapon;
 
-pub use actors::*;
+pub use animation_clips::*;
+pub use blocks_vision::*;
+pub use deflects_sounds::*;
+pub use door::*;
+pub use drop_shadow::*;
+pub use equipped_firearm::*;
+pub use footsteps::*;
+pub use guard::*;
+pub use hearing::*;
+pub use invisibility::*;
 pub use mover::*;
+pub use panning::*;
 pub use physics::*;
-pub use status_effects::*;
+pub use pickup::*;
+pub use player::*;
+pub use quiet_footsteps::*;
+pub use security_camera::*;
+pub use shatterable::*;
+pub use spawn_point::*;
+pub use start_location::*;
+pub use stunnable::*;
 pub use trigger::*;
+pub use vision::*;
+pub use wall::*;
+pub use waypoint::*;
+pub use weapon::*;
 
 pub(super) struct ComponentsPlugin;
 
 impl Plugin for ComponentsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((AgentsPlugin, MoverPlugin, PhysicsPlugin));
+        app.add_plugins((
+            GuardPlugin,
+            HearingPlugin,
+            MoverPlugin,
+            PhysicsPlugin,
+            VisionPlugin,
+        ));
     }
 }
--- src/components/status_effects/mod.rs
+++
@@ -1,5 +0,0 @@
-mod invisibility;
-mod quiet_footsteps;
-
-pub use invisibility::*;
-pub use quiet_footsteps::*;
