44957f19c4ad26e8880211a639f4cba617781d71
josh015 <joshua.d.ols@gmail.com>
2024-03-19 23:31:11 -0400

    Decouple "Security Camera" from `Vision`.

--- src/components/security_camera.rs
+++ src/components/security_camera.rs
@@ -3,3 +3,6 @@ use bevy::ecs::prelude::*;
 /// Designates a security camera entity.
 #[derive(Clone, Component, Debug, Default)]
 pub struct SecurityCamera;
+
+// TODO: SecurityCamera needs its own distinct vision component since it
+// behaves completely differently from guards.
