f3e68e22ddf503215c07d5c776cd93d17556e29e
josh015 <joshua.d.ols@gmail.com>
2024-03-21 07:56:17 -0400

    Combined `animation_clips` and `animation_entity_link` modules into `animation`.

--- src/components/animation_clips.rs
+++
@@ -1,5 +0,0 @@
-use bevy::{ecs::prelude::*, prelude::*, utils::HashMap};
-
-/// Stores animations for a given glTF scene.
-#[derive(Clone, Component, Debug, Default)]
-pub struct AnimationClips(pub HashMap<String, Handle<AnimationClip>>);
