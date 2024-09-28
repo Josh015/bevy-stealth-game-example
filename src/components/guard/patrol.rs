3bbbac2b1ff7a31841fe805022110c37a478db33
josh015 <joshua.d.ols@gmail.com>
2024-09-28 10:03:07 -0400

    Forgot to make patrol_location animation non_blocking.

--- src/components/guard/patrol.rs
+++ src/components/guard/patrol.rs
@@ -36,7 +36,7 @@ fn patrol_location(
         // Start "idle" animation (blocking, repeating).
 
         commands.actions(entity).add_many(actions![
-            AnimationAction::new("idle"),
+            AnimationAction::non_blocking("idle"),
             WaitAction::new(IDLE_DELAY),
             |agent: Entity, world: &mut World| -> bool {
                 world.entity_mut(agent).insert(Done::Success);
