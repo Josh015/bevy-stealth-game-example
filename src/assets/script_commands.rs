5a032dbf6ca1416e8b7b6a02bc0c7860c8c095a3
josh015 <joshua.d.ols@gmail.com>
2025-02-22 17:56:37 -0500

    Clarified notes on `ScriptCommand`.

--- src/assets/script_commands.rs
+++ src/assets/script_commands.rs
@@ -1,15 +1,14 @@
 pub enum ScriptCommands {
-    OpenDoor,   // Takes entity's map ID, non-blocking.
-    CloseDoor,  // Takes entity's map ID, non-blocking.
-    ToggleDoor, // Takes entity's map ID, non-blocking.
-    MoveTo,     // Takes waypoint's map ID, blocking.
-    LookAt,     // Takes waypoint's map ID, blocking.
-    Wait,       // Takes delay in seconds, blocking.
-    Repeat,     // non-blocking.
-    PlayAudio,  // Sound handle and text, non-blocking.
-    TickCount,  // Something to do with action synchronization?
-    Sync,       // blocking, blocks until both MoveTo and LookAt complete?
-    Reset,      // Resets a Trigger component on the entity.
-    SetDoorTimer, /* Shows door timer UI with countdown, takes countdown
-                 * time, non-blocking. */
+    OpenDoor,     // Takes entity's map ID, non-blocking.
+    CloseDoor,    // Takes entity's map ID, non-blocking.
+    ToggleDoor,   // Takes entity's map ID, non-blocking.
+    MoveTo,       // Takes waypoint's map ID, blocking.
+    LookAt,       // Takes waypoint's map ID, blocking.
+    Wait,         // Takes delay in seconds, blocking.
+    Repeat,       // non-blocking.
+    PlayAudio,    // Sound handle and text, non-blocking.
+    TickCount,    // Something to do with action synchronization?
+    Sync,         // blocking, blocks until both MoveTo and LookAt complete?
+    Reset,        // Used to reset floor switches to their off state.
+    SetDoorTimer, // Shows door timer UI, takes countdown time, non-blocking.
 }
